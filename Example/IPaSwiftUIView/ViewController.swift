eeccd2855e318ce76dc41df1b75d01a53e666a89
IPa Chen <ipapamagic@gmail.com>
2020-10-26 09:07:25 +0800

    Initial commit

---
+++ Example/IPaSwiftUIView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  IPaSwiftUIView
+//
+//  Created by IPa Chen on 10/26/2020.
+//  Copyright (c) 2020 IPa Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
